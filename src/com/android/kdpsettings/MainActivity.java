fc7cf0a23c10e3684ce4d4dc0525e94a3a462e54
KangDroid <hyunwoo286@gmail.com>
2017-01-13 19:59:44 +0900

    NEw start

--- src/com/android/kdpsettings/MainActivity.java
+++ src/com/android/kdpsettings/MainActivity.java
@@ -5,12 +5,22 @@ import android.app.FragmentTransaction;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.app.Activity;
+import android.widget.Toast;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.ListPreference;
+import android.preference.PreferenceScreen;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -20,7 +30,8 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import whdghks913.tistory.examplescrolltab.R;
+
+import com.android.kdpsettings.R;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
@@ -159,6 +170,22 @@ public class MainActivity extends FragmentActivity implements
 		 * fragment.
 		 */
 		private static final String ARG_SECTION_NUMBER = "section_number";
+		
+		private static final String TAG = "KangDroidAnimSettings";
+	
+		private static final String KEY_TOAST_ANIMATION = "toast_animation";
+	    private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
+	    private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
+	    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+	    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+	    private static final String SCROLLINGCACHE_DEFAULT = "1";
+	
+		private Context mContext;
+	
+		private ListPreference mToastAnimation;
+	    private ListPreference mListViewAnimation;
+	    private ListPreference mListViewInterpolator;
+		private ListPreference mScrollingCachePref;
 
 		public KangDroidTestFragment() {
 		}
@@ -179,7 +206,43 @@ public class MainActivity extends FragmentActivity implements
 		public void onCreate(Bundle savedInstanceState)
 		{
 			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.settings_test);
+			
+	        addPreferencesFromResource(R.xml.kangdroid_anim_settings);
+			PreferenceScreen prefSet = getPreferenceScreen();
+	        ContentResolver resolver = getActivity().getContentResolver();
+
+	        mContext = getActivity().getApplicationContext();
+
+	        // Toast Animations
+	        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+	        mToastAnimation.setSummary(mToastAnimation.getEntry());
+	        int CurrentToastAnimation = Settings.System.getInt(resolver,
+	                Settings.System.TOAST_ANIMATION, 1);
+	        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+	        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+	        mToastAnimation.setOnPreferenceChangeListener(this);
+		
+	        // List view animation
+	        mListViewAnimation = (ListPreference) findPreference(KEY_LISTVIEW_ANIMATION);
+	        int listviewanimation = Settings.System.getInt(resolver,
+	                Settings.System.LISTVIEW_ANIMATION, 0);
+	        mListViewAnimation.setValue(String.valueOf(listviewanimation));
+	        mListViewAnimation.setSummary(mListViewAnimation.getEntry());
+	        mListViewAnimation.setOnPreferenceChangeListener(this);
+
+	        mListViewInterpolator = (ListPreference) findPreference(KEY_LISTVIEW_INTERPOLATOR);
+	        int listviewinterpolator = Settings.System.getInt(resolver,
+	                Settings.System.LISTVIEW_INTERPOLATOR, 0);
+	        mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
+	        mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
+	        mListViewInterpolator.setOnPreferenceChangeListener(this);
+	        mListViewInterpolator.setEnabled(listviewanimation > 0);
+		
+	        // Scrolling cache
+	        mScrollingCachePref = (ListPreference) prefSet.findPreference(SCROLLINGCACHE_PREF);
+	        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+	                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+	        mScrollingCachePref.setOnPreferenceChangeListener(this);
 		}
 		
 	    @Override
@@ -188,8 +251,40 @@ public class MainActivity extends FragmentActivity implements
 	    }
 	
 	    public boolean onPreferenceChange(Preference preference, Object objValue) {
+	        final String key = preference.getKey();
+			ContentResolver resolver = getActivity().getContentResolver();
+	        if (preference == mToastAnimation) {
+	            int index = mToastAnimation.findIndexOfValue((String) objValue);
+	            Settings.System.putString(resolver, Settings.System.TOAST_ANIMATION, (String) objValue);
+	            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+	            Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+	            return true;
+	        }
+	        if (KEY_LISTVIEW_ANIMATION.equals(key)) {
+	            int value = Integer.parseInt((String) objValue);
+	            int index = mListViewAnimation.findIndexOfValue((String) objValue);
+	            Settings.System.putInt(resolver,
+	                    Settings.System.LISTVIEW_ANIMATION,
+	                    value);
+	            mListViewAnimation.setSummary(mListViewAnimation.getEntries()[index]);
+	            mListViewInterpolator.setEnabled(value > 0);
+	        }
+	        if (KEY_LISTVIEW_INTERPOLATOR.equals(key)) {
+	            int value = Integer.parseInt((String) objValue);
+	            int index = mListViewInterpolator.findIndexOfValue((String) objValue);
+	            Settings.System.putInt(resolver,
+	                    Settings.System.LISTVIEW_INTERPOLATOR,
+	                    value);
+	            mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
+	        }
+	        if (preference == mScrollingCachePref) {
+	            if (objValue != null) {
+	                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String)objValue);
+	            return true;
+	            }
+	        }
 	        return false;
-	    }
+	   }
 	}
 
 }
